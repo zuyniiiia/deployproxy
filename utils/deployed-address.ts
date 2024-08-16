0585443d6dc07ff454d40971bd24bbe4f760415f
zuyniiiia sym <phamduyen0005@gmail.com>
2024-08-16 04:43:21 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0xB553649082154c49c3d5C10892366175205622FE'
+export const ProxyAdmin = '0x608b6b248f96b80Ae2ecc2a2fdCC705a6672D9eb'
+export const SWTRImplementation = '0x37bFFeeB6450Ce63110CdA156eb58f0ef5ccc27c'
     
